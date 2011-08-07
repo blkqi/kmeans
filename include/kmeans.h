5a9cdfcf507cb3b886c71f6046ef4a23524daaf1
blkqi <brettftw@gmail.com>
2011-08-06 23:31:54 -0700

    move csv related defines to csv header

--- include/kmeans.h
+++ include/kmeans.h
@@ -1,8 +1,8 @@
 #ifndef __KMEANS_INCLUDED_H__
 #define __KMEANS_INCLUDED_H__
 
-#include <stdio.h>  // printf
 #include <math.h>   // fabs, INFINITY
+#include <stdio.h>  // printf
 #include <stdlib.h> 
 #include <string.h>
 #include <assert.h>
@@ -14,10 +14,6 @@
 //  Convergence tolerance
 //
 #   define TOL 1.0e-5
-
-//  -------------------------------------------------------------------------
-#   define MAX_N 2000  // maximum possible number of fields
-#   define MAX_M 32    // longest possible field + 1 = 31 byte field
 
 
 #endif
--- include/readcsv.h
+++ include/readcsv.h
@@ -1,7 +1,14 @@
 #ifndef __READCSV_INCLUDED_H__
 #define __READCSV_INCLUDED_H__
 
-//  read csv file into double array
+//  -------------------------------------------------------------------------
+//  Max size of data buffer
+//
+#   define MAX_N 2000  // maximum possible number of fields
+#   define MAX_M 32    // longest possible field + 1 = 31 byte field
+
+
+//  Read csv file into double array
 void 
     readcsv (char *file, double data[], int *n, int *m);
 
