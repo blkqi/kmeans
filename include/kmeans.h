ea8da7ae55fb361d6c15188e6f1449e897600dea
blkqi <brettftw@gmail.com>
2011-08-06 23:33:02 -0700

    re-org header

--- include/kmeans.h
+++ include/kmeans.h
@@ -1,14 +1,18 @@
 #ifndef __KMEANS_INCLUDED_H__
 #define __KMEANS_INCLUDED_H__
 
-#include <math.h>   // fabs, INFINITY
-#include <stdio.h>  // printf
-#include <stdlib.h> 
-#include <string.h>
-#include <assert.h>
+//  standard headers
+//
+#   include <math.h>   // fabs, INFINITY
+#   include <stdio.h>  // printf
+#   include <stdlib.h> 
+#   include <string.h>
+#   include <assert.h>
 
-#include "readcsv.h"
-#include "norm.h"
+//  project headers
+//
+#   include "readcsv.h"
+#   include "norm.h"
 
 //  -------------------------------------------------------------------------
 //  Convergence tolerance
